753d95319ac69c6d7b98aa6c727e3d726eddbc1c
valleg12 <victorienalleg@MacBook-Air-de-Victorien.local>
2025-05-27 12:27:13 +0200

    Initial commit: ajout de l'application support

---
+++ support/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BrandPresentation from './components/BrandPresentation';
+import { ThemeProvider } from './components/theme-provider';
+import { Toaster } from './components/ui/toaster';
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <div className="min-h-screen bg-background">
+          <Routes>
+            <Route path="/" element={<BrandPresentation />} />
+          </Routes>
+        </div>
+        <Toaster />
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
